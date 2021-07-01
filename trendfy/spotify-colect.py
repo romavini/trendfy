850de31e6949cde409762503bec002a88cdf6f5f
Vinícius Romano <mdcdxcvi@gmail.com>
2021-07-01 00:12:44 -0300

    Add get_track function.

--- trendfy/spotify-colect.py
+++ trendfy/spotify-colect.py
@@ -7,8 +7,24 @@ import json
 
 class Colect:
     def __init__(self):
-        styles = ["sertanejo", "samba", "bossa nova", "funk", "rap", "mpb"]
-        years = ["2016", "2017", "2018", "2019", "2020"]
+        styles = [
+            "sertanejo",
+            "samba",
+            "bossa nova",
+            "funk",
+            "rap",
+            "pop",
+            "mpb",
+            "rock",
+        ]
+        years = [
+            "2015",
+            "2016",
+            "2017",
+            "2018",
+            "2019",
+            "2020",
+        ]
 
         self.sp = spotipy.Spotify(
             client_credentials_manager=SpotifyClientCredentials(
@@ -18,7 +34,9 @@ class Colect:
         )
 
         df, result = self.get_playlists(styles, years)
-        self.write(df, result)
+        self.get_tracks(df)
+        # self.write(result)
+        # self.write(df, "df")
 
     def get_playlists(self, styles, years):
         styles_list = []
@@ -34,10 +52,9 @@ class Colect:
                 dict_styles["name"] = result["playlists"]["items"][0][
                     "name"
                 ].lower()
-                dict_styles["href"] = result["playlists"]["items"][0][
-                    "external_urls"
-                ]["spotify"]
+                dict_styles["id"] = result["playlists"]["items"][0]["id"]
                 dict_styles["style"] = style
+                dict_styles["year"] = year
 
                 styles_list.append(dict_styles)
 
@@ -45,14 +62,51 @@ class Colect:
 
         return df, result
 
-    def write(self, df, result):
-        with open("response.json", "w") as f:
-            json.dump(result, f)
+    def get_tracks(self, df):
+        print("Get Tracks")
+        track_list = []
 
-        with open("responsedf.json", "w") as f:
-            result_df = df.to_json(orient="split")
-            parsed = json.loads(result_df)
-            json.dump(parsed, f)
+        for idx in df.index:
+            print(f"{df.iloc[idx]['style']} {df.iloc[idx]['year']}")
+
+            playlist_id = df.iloc[idx]["id"]
+            result = self.sp.playlist(playlist_id)
+
+            for item in result["tracks"]["items"]:
+                tracks = {}
+                try:
+                    if item["track"]["track"]:
+                        tracks["track_name"] = item["track"]["name"]
+                        tracks["track_id"] = item["track"]["id"]
+                        tracks["artist_name"] = item["track"]["artists"][0][
+                            "name"
+                        ]
+                        tracks["artist_id"] = item["track"]["artists"][0]["id"]
+                        tracks["duration"] = item["track"]["duration_ms"]
+                        tracks["explicit"] = item["track"]["explicit"]
+                        tracks["popularity"] = item["track"]["popularity"]
+                        tracks["style"] = df.iloc[idx]["style"]
+                        tracks["year"] = df.iloc[idx]["year"]
+
+                        track_list.append(tracks)
+                except KeyError:
+                    print("[KeyError] -> Error in track")
+                except TypeError:
+                    print("[TypeError] -> Error in track")
+
+        self.write(track_list, filename="tracks")
+
+    @staticmethod
+    def write(data, type_data="json", filename="response"):
+        if type_data == "json":
+            with open(f"{filename}.json", "w") as f:
+                json.dump(data, f)
+
+        elif type_data == "df":
+            with open(f"{filename}.json", "w") as f:
+                result_df = data.to_json(orient="split")
+                parsed = json.loads(result_df)
+                json.dump(parsed, f)
 
 
 if __name__ == "__main__":
